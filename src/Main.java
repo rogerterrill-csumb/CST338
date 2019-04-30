93f210a8f7014ad01ea891581e8517a8497686df
Roger Terrill <rchicasterill@gmail.com>
2019-04-30 13:24:01 -0700

    Reformated with 3 spaces and new line for curly braces

--- src/Main.java
+++ src/Main.java
@@ -11,51 +11,53 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-public class Main {
-    public static final int MIN_HOURS = 12;
-    public static final int MAX_HOURS = 20;
+public class Main
+{
+   public static final int MIN_HOURS = 12;
+   public static final int MAX_HOURS = 20;
 
-    public static void main(String[] args) {
+   public static void main(String[] args)
+   {
 
-        // Create an Scanner object
-        Scanner keyboard = new Scanner(System.in);
+      // Create an Scanner object
+      Scanner keyboard = new Scanner(System.in);
 
-        DecimalFormat formattingObject = new DecimalFormat("00.0");
+      DecimalFormat formattingObject = new DecimalFormat("00.0");
 
-        // Prompt user to enter first and last name
-        System.out.println("Please enter your first and last name.");
-        System.out.println("Please capitalize the first letter for both.");
+      // Prompt user to enter first and last name
+      System.out.println("Please enter your first and last name.");
+      System.out.println("Please capitalize the first letter for both.");
 
-        // Assign both to specific variables
-        String firstName = keyboard.next();
-        String lastName = keyboard.next();
+      // Assign both to specific variables
+      String firstName = keyboard.next();
+      String lastName = keyboard.next();
 
-        // Concatenate first and last name
-        String fullName = firstName + " " + lastName;
+      // Concatenate first and last name
+      String fullName = firstName + " " + lastName;
 
-        // Return the length of the concatenated string
-        int fullNameLength = fullName.length();
+      // Return the length of the concatenated string
+      int fullNameLength = fullName.length();
 
-        // Display concatenated full name
-        System.out.println("Your full name is: " + fullName + " and is " + fullNameLength + " long.");
+      // Display concatenated full name
+      System.out.println("Your full name is: " + fullName + " and is " + fullNameLength + " long.");
 
-        // Display capitalized full name
-        System.out.println("Your full name capitalized: " + fullName.toUpperCase());
+      // Display capitalized full name
+      System.out.println("Your full name capitalized: " + fullName.toUpperCase());
 
-        // Display lowercased full name
-        System.out.println("Your full name lowercase: " + fullName.toLowerCase());
+      // Display lowercased full name
+      System.out.println("Your full name lowercase: " + fullName.toLowerCase());
 
-        // Display prompt asking user how many hours they have spent this week
-        System.out.println("Hi " + fullName + ", how many hours have you spent on class this week? ");
-        System.out.println("The values should be between " + MIN_HOURS + " and " + MAX_HOURS);
-        System.out.println("Also use 3 decimal places as well.");
+      // Display prompt asking user how many hours they have spent this week
+      System.out.println("Hi " + fullName + ", how many hours have you spent on class this week? ");
+      System.out.println("The values should be between " + MIN_HOURS + " and " + MAX_HOURS);
+      System.out.println("Also use 3 decimal places as well.");
 
-        // Assign hours to variable
-        double hoursSpentOnClass = keyboard.nextDouble();
+      // Assign hours to variable
+      double hoursSpentOnClass = keyboard.nextDouble();
 
-        // Display the hours entered to one decimal place
-        System.out.println("You have spent approximately " + formattingObject.format(hoursSpentOnClass) + " hours this week.");
-    }
+      // Display the hours entered to one decimal place
+      System.out.println("You have spent approximately " + formattingObject.format(hoursSpentOnClass) + " hours this week.");
+   }
 }
 
 /***************************Output*********************************
