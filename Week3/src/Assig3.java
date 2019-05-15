66c7a4367c26ca590acbe9013fbfa400dff60290
Roger Terrill <rchicasterill@gmail.com>
2019-05-15 16:28:31 -0700

    Phase 1 in progress

---
+++ Week3/src/Assig3.java
@@ -0,0 +1,70 @@
+public class Assig3
+{
+
+    public static void main(String[] args) {
+        Card one = new Card();
+        one.toString();
+    }
+
+
+}
+
+class Card
+{
+    enum Suit{ clubs, diamonds, hearts, spades };
+    char value;
+    Suit suit;
+    boolean errorFlag;
+
+    public Card()
+    {
+        value = 'A';
+        suit = Suit.spades;
+    }
+
+    public Card(char value, Suit suit)
+    {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public boolean set(char value, Suit suit)
+    {
+
+        return true;
+    }
+
+
+    public String toString()
+    {
+        String str = "";
+        System.out.println(value + " of " + suit);
+        return str;
+    }
+
+    public char getValue()
+    {
+        return value;
+    }
+
+    public Suit getSuit()
+    {
+        return suit;
+    }
+
+    public boolean isErrorFlag()
+    {
+        return errorFlag;
+    }
+
+    public boolean equals(Card card)
+    {
+        return true;
+    }
+
+    private boolean isValid(char value, Suit suit)
+    {
+
+        return true;
+    }
+}
