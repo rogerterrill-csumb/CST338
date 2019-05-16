d04681573d34531d0f050fd2e76bde633c30304f
Roger Terrill <rchicasterill@gmail.com>
2019-05-16 08:00:32 -0700

    Save old file

--- Week3/src/Assig3.java
+++ Week3/src/Assig3.java
@@ -1,70 +1,70 @@
 public class Assig3
 {
 
-    public static void main(String[] args) {
-        Card one = new Card();
-        one.toString();
-    }
+   public static void main(String[] args) {
+      Card one = new Card();
+      one.toString();
+   }
 
 
 }
 
 class Card
 {
-    enum Suit{ clubs, diamonds, hearts, spades };
-    char value;
-    Suit suit;
-    boolean errorFlag;
+   enum Suit{ clubs, diamonds, hearts, spades };
+   char value;
+   Suit suit;
+   boolean errorFlag;
 
-    public Card()
-    {
-        value = 'A';
-        suit = Suit.spades;
-    }
+   public Card()
+   {
+      value = 'A';
+      suit = Suit.spades;
+   }
 
-    public Card(char value, Suit suit)
-    {
-        this.value = value;
-        this.suit = suit;
-    }
+   public Card(char value, Suit suit)
+   {
+      this.value = value;
+      this.suit = suit;
+   }
 
-    public boolean set(char value, Suit suit)
-    {
+   public boolean set(char value, Suit suit)
+   {
 
-        return true;
-    }
+      return true;
+   }
 
 
-    public String toString()
-    {
-        String str = "";
-        System.out.println(value + " of " + suit);
-        return str;
-    }
+   public String toString()
+   {
+      String str = "";
+      System.out.println(value + " of " + suit);
+      return str;
+   }
 
-    public char getValue()
-    {
-        return value;
-    }
+   public char getValue()
+   {
+      return value;
+   }
 
-    public Suit getSuit()
-    {
-        return suit;
-    }
+   public Suit getSuit()
+   {
+      return suit;
+   }
 
-    public boolean isErrorFlag()
-    {
-        return errorFlag;
-    }
+   public boolean isErrorFlag()
+   {
+      return errorFlag;
+   }
 
-    public boolean equals(Card card)
-    {
-        return true;
-    }
+   public boolean equals(Card card)
+   {
+      return true;
+   }
 
-    private boolean isValid(char value, Suit suit)
-    {
+   private boolean isValid(char value, Suit suit)
+   {
 
-        return true;
-    }
+      return true;
+   }
 }
