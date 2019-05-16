7e4f2555711f2f28a1ac0c75ebfda3be0169551a
Roger Dodger <lpaddikt@gmail.com>
2019-05-15 22:47:51 -0700

    Added access modifiers to members, updated toString to check for errorFlag

--- Week3/src/Assig3.java
+++ Week3/src/Assig3.java
@@ -1,70 +1,73 @@
 public class Assig3
 {
 
-    public static void main(String[] args) {
-        Card one = new Card();
-        one.toString();
-    }
+   public static void main(String[] args)
+   {
+      Card one = new Card();
+      System.out.print(one.toString());
+   }
 
 
 }
 
 class Card
 {
-    enum Suit{ clubs, diamonds, hearts, spades };
-    char value;
-    Suit suit;
-    boolean errorFlag;
+   enum Suit {clubs, diamonds, hearts, spades}
 
-    public Card()
-    {
-        value = 'A';
-        suit = Suit.spades;
-    }
+   private char value;
+   private Suit suit;
+   private boolean errorFlag;
 
-    public Card(char value, Suit suit)
-    {
-        this.value = value;
-        this.suit = suit;
-    }
+   public Card()
+   {
+      value = 'A';
+      suit = Suit.spades;
+   }
 
-    public boolean set(char value, Suit suit)
-    {
+   public Card(char value, Suit suit)
+   {
+      this.value = value;
+      this.suit = suit;
+   }
 
-        return true;
-    }
+   public String toString()
+   {
+      if(errorFlag == true)
+      {
+         return "[invalid]";
+      }
+      return value + " of " + suit ;
+   }
 
+   public boolean set(char value, Suit suit)
+   {
 
-    public String toString()
-    {
-        String str = "";
-        System.out.println(value + " of " + suit);
-        return str;
-    }
+      return true;
+   }
 
-    public char getValue()
-    {
-        return value;
-    }
+   public char getValue()
+   {
+      return value;
+   }
 
-    public Suit getSuit()
-    {
-        return suit;
-    }
+   public Suit getSuit()
+   {
+      return suit;
+   }
 
-    public boolean isErrorFlag()
-    {
-        return errorFlag;
-    }
+   public boolean isErrorFlag()
+   {
+      return errorFlag;
+   }
 
-    public boolean equals(Card card)
-    {
-        return true;
-    }
+   public boolean equals(Card card)
+   {
+      return true;
+   }
 
-    private boolean isValid(char value, Suit suit)
-    {
+   private boolean isValid(char value, Suit suit)
+   {
 
-        return true;
-    }
+      return true;
+   }
 }
