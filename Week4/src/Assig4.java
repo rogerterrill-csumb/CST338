0f09e347a251cd2e024de706777a30cc07ffdc73
Roger Terrill <rchicasterill@gmail.com>
2019-05-22 15:19:00 -0700

    Phase 2: cloneable done and displayToConsole()

--- Week4/src/Assig4.java
+++ Week4/src/Assig4.java
@@ -16,30 +16,35 @@ public class Assig4
 
       String[] myString = {"    **", "  **  "};
       BarcodeImage newCode = new BarcodeImage(myString);
-      BarcodeImage copy = (BarcodeImage) newCode.clone();
-      System.out.println(newCode.getPixel(0,5));
-      System.out.println(copy.getPixel(0,5));
-      BarcodeImage copy2 = newCode;
+      BarcodeImage copy = (BarcodeImage)newCode.clone();
+      System.out.println(newCode.getPixel(0, 5)); //True
+      System.out.println(copy.getPixel(0, 5)); //True
       copy.setPixel(0,5,false);
-      System.out.println(newCode.getPixel(0,5));
-      System.out.println(copy.getPixel(0,5));
+      System.out.println(newCode.getPixel(0, 5)); //False
+      System.out.println(copy.getPixel(0, 5)); //False
+      newCode.displayToConsole();
    }
 
 
 }
 
-// Phase 1 BarcodIO interface
+// Phase 1 BarcodeIO interface
 interface BarcodeIO
 {
    public boolean scan(BarcodeImage bc);
+
    public boolean readText(String text);
+
    public boolean generateImageFromText();
+
    public boolean translateImageToText();
+
    public void displayTextToConsole();
+
    public void displayImageToConsole();
 }
 
-
+// Phase 2 BarcodeImage class
 class BarcodeImage implements Cloneable
 {
    // DATA
@@ -63,7 +68,7 @@ class BarcodeImage implements Cloneable
 
    public BarcodeImage(String[] strData)
    {
-      if(checkSize(strData))
+      if (checkSize(strData))
       {
          imageData = new boolean[MAX_HEIGHT][MAX_WIDTH];
          int i, j;
@@ -75,24 +80,23 @@ class BarcodeImage implements Cloneable
             }
          }
 
-         for(i = 0; i < strData.length; i++)
+         for (i = 0; i < strData.length; i++)
          {
-            for(j = 0; j < strData[i].length(); j++)
+            for (j = 0; j < strData[i].length(); j++)
             {
-               if(strData[i].charAt(j) == ' ')
-                  setPixel(i,j,false);
+               if (strData[i].charAt(j) == ' ')
+                  setPixel(i, j, false);
 
                else if (strData[i].charAt(j) == '*')
-                  setPixel(i,j,true);
+                  setPixel(i, j, true);
             }
          }
       }
-
    }
 
    public boolean getPixel(int row, int col)
    {
-      if(imageData != null)
+      if (imageData != null)
       {
          return imageData[row][col];
       }
@@ -101,7 +105,7 @@ class BarcodeImage implements Cloneable
 
    public boolean setPixel(int row, int col, boolean value)
    {
-      if(row < MAX_WIDTH && col < MAX_HEIGHT)
+      if (row < MAX_WIDTH && col < MAX_HEIGHT)
       {
          imageData[row][col] = value;
          return true;
@@ -109,32 +113,50 @@ class BarcodeImage implements Cloneable
       return false;
    }
 
-    // OPTIONAL PRIVATE HELPER METHODS
-    private boolean checkSize(String[] data)
-    {
-       if (data != null)
-         return data.length < MAX_HEIGHT && data[0].length()<MAX_WIDTH;
-       return false;
-    }
+   // OPTIONAL PRIVATE HELPER METHODS
+   private boolean checkSize(String[] data)
+   {
+      if (data != null)
+         return data.length < MAX_HEIGHT && data[0].length() < MAX_WIDTH;
+      return false;
+   }
 
-//    public void displayToConsole()
-//    {
-//
-//    }
-//
+   public void displayToConsole()
+   {
+      for(boolean[] i : imageData)
+      {
+         // Going through each row
+         for(boolean j: i)
+         {
+            // Knows that element i is also an iterable since it's a String
+            if(j)
+            {
+               System.out.print('*');
+            }
+            else
+            {
+               System.out.print(' ');
+            }
+         }
+         System.out.println();
+      }
+   }
+
    @Override
-    public Object clone()
-    {
-       try
-       {
-          return super.clone();
-       }
-       catch(CloneNotSupportedException e)
-       {
-          return null;
-       }
+   public Object clone()
+   {
+      try
+      {
+         BarcodeImage copy = (BarcodeImage)super.clone();
+         copy.imageData = (boolean[][])imageData.clone();
+         return copy;
+      }
+      catch (CloneNotSupportedException e)
+      {
+         return null;
+      }
 
-    }
+   }
 }
 
 
