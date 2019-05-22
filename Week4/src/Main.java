e6c32e8f391be07498fb14caf488234b700d3d47
Roger Dodger <lpaddikt@gmail.com>
2019-05-21 21:05:15 -0700

    Started phase 2, currently working on default constructor

--- Week4/src/Main.java
+++ Week4/src/Main.java
@@ -26,3 +26,106 @@ interface BarcodeIO
     public void displayTextToConsole();
     public void displayImageToConsole();
 }
+
+
+class BarcodeImage implements Cloneable
+{
+    // DATA
+    public static final int MAX_HEIGHT = 30;
+    public static final int MAX_WIDTH = 65;
+    private boolean[][] imageData;
+
+    // METHOD
+    public BarcodeImage()
+    {
+        int i,j;
+        for(i=0;i<MAX_WIDTH;i++)
+        {
+            for(j=0;j<MAX_HEIGHT;i++)
+            {
+                imageData[i][j] = false;
+            }
+        }
+    }
+
+    public BarcodeImage(String[] strData)
+    {
+
+    }
+
+    public boolean getPixel(int row, int col)
+    {
+
+    }
+
+    public boolean setPixel(int row, int col, boolean value)
+    {
+
+    }
+
+    // PRIVATE UTILITY METHOD
+    private void checkSize(String[] data)
+    {
+
+    }
+
+    private void displayToConsole()
+    {
+
+    }
+
+    public void clone()
+    {
+
+    }
+}
+
+
+//class DataMatrix implements BarcodeIO
+//{
+//    // METHODS
+//    public DataMatrix()
+//    {
+//
+//    }
+//
+//    public DataMatrix(BarcodeImage image)
+//    {
+//
+//    }
+//
+//    public DataMatrix(String text)
+//    {
+//
+//    }
+//
+//    public readText(String text)
+//    {
+//
+//    }
+//
+//    public scan(BarcodeImage image)
+//    {
+//
+//    }
+//
+//    public actualWidth()
+//    {
+//
+//    }
+//
+//    public actualHeight()
+//    {
+//
+//    }
+//
+//    private int computeSignalWidth()
+//    {
+//
+//    }
+//
+//    private  int computeSignalHeight()
+//    {
+//
+//    }
+//}
