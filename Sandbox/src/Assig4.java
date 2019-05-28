e73523a32fd5304090f7e568cdc40bec98c366fc
Roger Terrill <rchicasterill@gmail.com>
2019-05-28 11:45:52 -0700

    Normal way to define a JFrame

--- Sandbox/src/Assig4.java
+++ Sandbox/src/Assig4.java
@@ -5,22 +5,28 @@ import java.awt.event.ActionEvent;
 
 
 class Assig4 {
+
+    public static void main(String[] args)
+    {
+        FirstWindow w = new FirstWindow();
+        w.setVisible(true);
+    }
+}
+
+class FirstWindow extends JFrame
+{
     public static final int WIDTH = 300;
     public static final int HEIGHT = 200;
 
-
-    public static void main(String[] args) {
-        JFrame firstWindow = new JFrame();
-        firstWindow.setSize(WIDTH, HEIGHT);
-        firstWindow.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
-        JButton endButton = new JButton("Click to end program.");
-        EndingListener buttonEar = new EndingListener();
-        endButton.addActionListener(buttonEar);
-        firstWindow.add(endButton);
-
-        firstWindow.setVisible(true);
-
+    public FirstWindow()
+    {
+        super();
+        setSize(WIDTH, HEIGHT);
+        setTitle("First Window Class");
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        JButton endButton = new JButton("Click to end program");
+        endButton.addActionListener(new EndingListener());
+        add(endButton);
     }
 }
 
