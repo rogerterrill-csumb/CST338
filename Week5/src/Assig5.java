c183a78ed55df52261244185f670fb52e08238dc
Roger Terrill <rchicasterill@gmail.com>
2019-05-29 11:55:09 -0700

    Reformatted to be 3 spaces instead of 4

--- Week5/src/Assig5.java
+++ Week5/src/Assig5.java
@@ -3,81 +3,82 @@ import java.awt.*;
 
 public class Assig5
 {
-    // static for the 57 icons and their corresponding labels
-    // normally we would not have a separate label for each card, but
-    // if we want to display all at once using labels, we need to.
+   // static for the 57 icons and their corresponding labels
+   // normally we would not have a separate label for each card, but
+   // if we want to display all at once using labels, we need to.
 
-    static final int NUM_CARD_IMAGES = 57; // 52 + 4 jokers + 1 back-of-card image
-    static Icon[] icon = new ImageIcon[NUM_CARD_IMAGES];
+   static final int NUM_CARD_IMAGES = 57; // 52 + 4 jokers + 1 back-of-card image
+   static Icon[] icon = new ImageIcon[NUM_CARD_IMAGES];
 
-    static void loadCardIcons()
-    {
-        // build the file names ("AC.gif", "2C.gif", "3C.gif", "TC.gif", etc.)
-        // in a SHORT loop.  For each file name, read it in and use it to
-        // instantiate each of the 57 Icons in the icon[] array.
-        String str = "";
+   static void loadCardIcons()
+   {
+      // build the file names ("AC.gif", "2C.gif", "3C.gif", "TC.gif", etc.)
+      // in a SHORT loop.  For each file name, read it in and use it to
+      // instantiate each of the 57 Icons in the icon[] array.
+      String str = "";
 
-        // Directory prefix to take into account different IDEs
-        String directoryPrefix = "images\\";
+      // Directory prefix to take into account different IDEs
+      String directoryPrefix = "images\\";
 
-        for(int i = 0; i < NUM_CARD_IMAGES; i++)
-        {
-            // Checks for last card and sets it to back of card and then breaks out of loop.
-            if(i == NUM_CARD_IMAGES - 1)
-            {
-                str = directoryPrefix + "BK.gif";
-                icon[i] = new ImageIcon(str);
-                break;
-            }
-            str = directoryPrefix + turnIntIntoCardValue(i%14) + turnIntIntoCardSuit(i/14) + ".gif";
+      for (int i = 0; i < NUM_CARD_IMAGES; i++)
+      {
+         // Checks for last card and sets it to back of card and then breaks out of loop.
+         if (i == NUM_CARD_IMAGES - 1)
+         {
+            str = directoryPrefix + "BK.gif";
             icon[i] = new ImageIcon(str);
-        }
+            break;
+         }
+         str = directoryPrefix + turnIntIntoCardValue(i % 14) + turnIntIntoCardSuit(i / 14) + ".gif";
+         icon[i] = new ImageIcon(str);
+      }
 
-    }
-    // turns 0 - 13 into "A", "2", "3", ... "Q", "K", "X"
-    static String turnIntIntoCardValue(int k)
-    {
-        // an idea for a helper method (do it differently if you wish)
-        String[] cardValues = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "X"};
-        return cardValues[k];
-    }
+   }
 
-    // turns 0 - 3 into "C", "D", "H", "S"
-    static String turnIntIntoCardSuit(int j)
-    {
-        // an idea for another helper method (do it differently if you wish)
-        String[] cardSuits = {"C", "D", "H", "S"};
-        return cardSuits[j];
-    }
+   // turns 0 - 13 into "A", "2", "3", ... "Q", "K", "X"
+   static String turnIntIntoCardValue(int k)
+   {
+      // an idea for a helper method (do it differently if you wish)
+      String[] cardValues = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "X"};
+      return cardValues[k];
+   }
 
-    // a simple main to throw all the JLabels out there for the world to see
-    public static void main(String[] args)
-    {
-        int k;
+   // turns 0 - 3 into "C", "D", "H", "S"
+   static String turnIntIntoCardSuit(int j)
+   {
+      // an idea for another helper method (do it differently if you wish)
+      String[] cardSuits = {"C", "D", "H", "S"};
+      return cardSuits[j];
+   }
 
-        // prepare the image icon array
-        loadCardIcons();
+   // a simple main to throw all the JLabels out there for the world to see
+   public static void main(String[] args)
+   {
+      int k;
 
-        // establish main frame in which program will run
-        JFrame frmMyWindow = new JFrame("Card Room");
-        frmMyWindow.setSize(1150, 650);
-        frmMyWindow.setLocationRelativeTo(null);
-        frmMyWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      // prepare the image icon array
+      loadCardIcons();
 
-        // set up layout which will control placement of buttons, etc.
-        FlowLayout layout = new FlowLayout(FlowLayout.CENTER, 5, 20);
-        frmMyWindow.setLayout(layout);
+      // establish main frame in which program will run
+      JFrame frmMyWindow = new JFrame("Card Room");
+      frmMyWindow.setSize(1150, 650);
+      frmMyWindow.setLocationRelativeTo(null);
+      frmMyWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // prepare the image label array
-        JLabel[] labels = new JLabel[NUM_CARD_IMAGES];
-        for (k = 0; k < NUM_CARD_IMAGES; k++)
-            labels[k] = new JLabel(icon[k]);
+      // set up layout which will control placement of buttons, etc.
+      FlowLayout layout = new FlowLayout(FlowLayout.CENTER, 5, 20);
+      frmMyWindow.setLayout(layout);
 
-        // place your 3 controls into frame
-        for (k = 0; k < NUM_CARD_IMAGES; k++)
-            frmMyWindow.add(labels[k]);
+      // prepare the image label array
+      JLabel[] labels = new JLabel[NUM_CARD_IMAGES];
+      for (k = 0; k < NUM_CARD_IMAGES; k++)
+         labels[k] = new JLabel(icon[k]);
 
-        // show everything to the user
-        frmMyWindow.setVisible(true);
-    }
+      // place your 3 controls into frame
+      for (k = 0; k < NUM_CARD_IMAGES; k++)
+         frmMyWindow.add(labels[k]);
+
+      // show everything to the user
+      frmMyWindow.setVisible(true);
+   }
 }
