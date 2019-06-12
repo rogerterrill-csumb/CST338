7a671fe11c99608b07f0fae83ed67cafe1527f7a
Roger Terrill <rchicasterill@gmail.com>
2019-06-12 12:09:03 -0700

    Added text and color

--- Week7/app/src/main/java/com/example/week7/MainActivity.java
+++ Week7/app/src/main/java/com/example/week7/MainActivity.java
@@ -10,48 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Life Cycle Event: ", "In onCreate()");
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Log.d("Life Cycle Event: ", "In onStart()");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Log.d("Life Cycle Event: ", "In onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Log.d("Life Cycle Event: ", "In onPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Log.d("Life Cycle Event: ", "In onStop()");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Log.d("Life Cycle Event: ", "In onDestroy()");
-
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("Life Cycle Event: ", "In onRestart()");
-    }
 }
