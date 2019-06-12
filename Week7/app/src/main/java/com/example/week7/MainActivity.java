751e49a0746d80ea5887c255af0dfb326d960563
Roger Terrill <rchicasterill@gmail.com>
2019-06-12 11:34:57 -0700

    Life Cycle Example

--- Week7/app/src/main/java/com/example/week7/MainActivity.java
+++ Week7/app/src/main/java/com/example/week7/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.week7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Life Cycle Event: ", "In onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("Life Cycle Event: ", "In onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("Life Cycle Event: ", "In onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("Life Cycle Event: ", "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("Life Cycle Event: ", "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("Life Cycle Event: ", "In onDestroy()");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Life Cycle Event: ", "In onRestart()");
     }
 }
