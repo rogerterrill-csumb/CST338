6955c9c30cab3b06d427dc6d63eb7026fb0c8956
Roger Dodger <lpaddikt@gmail.com>
2019-06-12 20:38:38 -0700

    Starting Final Project

---
+++ Week8/src/GameModel.java
@@ -0,0 +1,293 @@
+class GameModel
+{
+   // Global Constants
+   static int NUM_CARDS_PER_HAND = 7;
+   static int NUM_PLAYERS = 2;
+   static int PAUSE = 1000;
+
+   // Private members that hold the winning cards
+   private Card[] compWinnings = new Card[NUM_PLAYERS * NUM_CARDS_PER_HAND];
+   private Card[] playerWinnings = new Card[NUM_PLAYERS * NUM_CARDS_PER_HAND];
+
+   // Status if players turn
+   private boolean playersTurn = true;
+
+   // Status of players scores
+   private int computerScore = 0;
+   private int playerScore = 0;
+
+   // Status of cards won in each hand
+   private int computerCardsWon = 0;
+   private int playerCardsWon = 0;
+
+   // Status of the text depending on win or loss
+   private String playerStatus = "";
+
+   // Computer card index counter
+   private int computerCardCounter = 0;
+
+   // Seconds of timer
+   private int seconds;
+
+   // Current card values
+   private Card playerCard;
+   private Card computerCard;
+
+   // Member that holds GameCardFramework object and it's arguments
+   private CardGameFramework highCardGame;
+   private int numPacksPerDeck = 1;
+   private int numJokersPerPack = 2;
+   private int numUnusedCardsPerPack = 0;
+   private Card[] unusedCardsPerPack = null;
+
+   // Timer status
+   private Boolean timeStatus = false;
+
+   GameModel()
+   {
+      // Initialize a new CardGameFramework
+      highCardGame = new CardGameFramework(numPacksPerDeck,numJokersPerPack,numUnusedCardsPerPack,unusedCardsPerPack,NUM_PLAYERS,NUM_CARDS_PER_HAND);
+
+      // Deal the cards from CardGameFramework
+      highCardGame.deal();
+
+      // Set the first initial computer card to display
+      computerCard = highCardGame.getCardFromDeck();
+
+      // Sets the first initial player card
+      playerCard = highCardGame.getCardFromDeck();
+   }
+
+   // Deal two cards onto the pile
+   public void dealCardsToPile()
+   {
+      // Set the first initial computer card to display
+      computerCard = highCardGame.getCardFromDeck();
+
+      // Sets the first initial player card
+      playerCard = highCardGame.getCardFromDeck();
+   }
+
+   // Compare player card value to the board card value
+   public boolean playerCardCheck(Card card)
+   {
+      int diff = Card.valueOfCard(playerCard) - Card.valueOfCard(card);
+      // If player wins
+      if( diff == 1 || diff == -1)
+      {
+         return true;
+      }
+      return false;
+   }
+
+   // Compare player card value to the board card value
+   public boolean computerCardCheck(Card card)
+   {
+      int diff = Card.valueOfCard(computerCard) - Card.valueOfCard(card);
+      // If player wins
+      if( diff == 1 || diff == -1)
+      {
+         return true;
+      }
+      return false;
+   }
+
+   // Getters and Setters
+   public CardGameFramework getHighCardGame()
+   {
+      return highCardGame;
+   }
+
+   public void setPlayerCard(Card playerCard)
+   {
+      this.playerCard = playerCard;
+   }
+
+
+   public void setComputerCard(Card computerCard)
+   {
+      this.computerCard = computerCard;
+   }
+
+   public Card getComputerCard()
+   {
+      return computerCard;
+   }
+
+   public Card getPlayerCard()
+   {
+      return playerCard;
+   }
+
+   // Updates the computerCard with the new computerCardCounter
+   public void updateComputerCard()
+   {
+      this.computerCard = highCardGame.getHand(0).inspectCard(computerCardCounter);
+   }
+
+   // DEBUG: Prints the string of both cards
+   public void printCards()
+   {
+      System.out.println("Your Card " + playerCard.toString() + " and the Computer Card " + computerCard.toString());
+   }
+
+   // Increments seconds
+   public void incrementSecondsonTimer()
+   {
+      seconds++;
+   }
+
+   // Pause for a second the thread
+   public void doNothing(int milliseconds)
+   {
+      try
+      {
+         Thread.sleep(milliseconds);
+      }
+      catch (InterruptedException e)
+      {
+         System.out.println("Unexpected interrupt");
+         System.exit(0);
+      }
+   }
+
+   // Gets the status of the timer boolean
+   public Boolean getTimeStatus()
+   {
+      return timeStatus;
+   }
+
+   // Sets the status of the timer boolean
+   public void setTimeStatus(Boolean timeStatus)
+   {
+      this.timeStatus = timeStatus;
+   }
+
+   // Gets the current seconds
+   public int getSeconds()
+   {
+      return seconds;
+   }
+
+   // Computer and Player Score Getters and Setters
+   public static int getNumCardsPerHand()
+   {
+      return NUM_CARDS_PER_HAND;
+   }
+
+   public static void setNumCardsPerHand(int numCardsPerHand)
+   {
+      NUM_CARDS_PER_HAND = numCardsPerHand;
+   }
+
+   public int getComputerScore()
+   {
+      return computerScore;
+   }
+
+   public void setComputerScore(int computerScore)
+   {
+      this.computerScore = computerScore;
+   }
+
+   public int getPlayerScore()
+   {
+      return playerScore;
+   }
+
+   public void setPlayerScore(int playerScore)
+   {
+      this.playerScore = playerScore;
+   }
+
+   public String getGameStatusWithScores()
+   {
+      return Integer.toString(playerScore) + " - " + Integer.toString(computerScore) + " \n" + playerStatus;
+   }
+
+   public String displayFinalScore()
+   {
+      if(playerScore > computerScore)
+      {
+         return Integer.toString(playerScore) + " - " + Integer.toString(computerScore) + " \n YOU LOST!";
+      }
+      else if(playerScore < computerScore)
+      {
+         return Integer.toString(playerScore) + " - " + Integer.toString(computerScore) + " \n YOU WON!.";
+      }
+      else
+      {
+         return Integer.toString(playerScore) + " - " + Integer.toString(computerScore) + " \n DRAW!";
+      }
+
+   }
+
+   public String getPlayerStatus()
+   {
+      return playerStatus;
+   }
+
+   public void setPlayerStatus(String playerStatus)
+   {
+      this.playerStatus = playerStatus;
+   }
+
+   public void incrementComputerScore()
+   {
+      computerScore++;
+   }
+
+   public void incrementPlayerScore()
+   {
+      playerScore++;
+   }
+
+   public void takeCardFromDeckforPlayer()
+   {
+      playerCard = highCardGame.playCard(0, 1);
+   }
+
+   public boolean isPlayersTurn()
+   {
+      return playersTurn;
+   }
+
+   public void setPlayersTurn(boolean playersTurn)
+   {
+      this.playersTurn = playersTurn;
+   }
+
+   /**
+    * DEBUGGING METHODS
+    */
+   // DEBUG: Displays the int card values of both cards
+   public void displayValueComparison()
+   {
+      System.out.println("Player card value " + Card.valueOfCard(playerCard));
+      System.out.println("Computer card value " + Card.valueOfCard(computerCard));
+   }
+
+   // DEBUG: Displays the full player winnings
+   public void displayPlayerWinnings()
+   {
+      for(int card = 0; card < playerCardsWon; card++)
+      {
+         System.out.println(playerWinnings[card].toString());
+      }
+   }
+
+   // DEBUG: Displays the full computer winnings
+   public void displayComputerWinnings()
+   {
+      for(int card = 0; card < computerCardsWon; card++)
+      {
+         System.out.println(compWinnings[card].toString());
+      }
+   }
+
+   // DEBUG: Displays the hand of either computer or player
+   public void displayHand(int hand)
+   {
+      System.out.println(highCardGame.getHand(hand).toString());
+   }
+}
---
+++ Week8/src/GameView.java
@@ -0,0 +1,273 @@
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+class GameView extends JFrame
+{
+   // Constants
+   private int NUM_CARDS_PER_HAND = GameModel.NUM_CARDS_PER_HAND;
+   private int NUM_PLAYERS = GameModel.NUM_PLAYERS;
+
+   // CardTable private members
+   private int numCardsPerHand;
+   private int numPlayers;
+
+   // Cards to be displayed
+   private Card computerCard;
+   private Card playerCard;
+
+
+   // JLabels private members to are the Cards Displayed
+   private JLabel[] computerLabels = new JLabel[NUM_CARDS_PER_HAND];
+   private JLabel[] playedCardLabels = new JLabel[NUM_PLAYERS];
+   private JButton[] playerCardButtons = new JButton[NUM_CARDS_PER_HAND];
+   private JButton cannotPlayButton = new JButton("I Cannot Play");
+
+   // JLabels private members that display text
+   private JLabel gameText = new JLabel();
+   private JLabel gameStatus = new JLabel();
+   private JLabel playerCardLabel, computerCardLabel;
+
+   // Timer Display Components
+   private JLabel timerDisplay = new JLabel("0:00", SwingConstants.CENTER);
+   private JButton startButton = new JButton("Start");
+
+   // 4 panels - One Computer player, One Human player, One play area, Timer Area
+   private JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea, pnlTimerDisplay;
+
+   // CardGameFramework object to pass into from model through controller
+   private CardGameFramework highCardGame;
+
+   GameView()
+   {
+      // Sets the title of the blank table
+      super("Card Game Table");
+
+      // Foundational Methods to Setup Frame
+      setSize(800, 700);
+      setLocationRelativeTo(null);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      // Show the table
+      setVisible(true);
+   }
+
+   public void initTable()
+   {
+      // Container Layout Manager
+      setLayout(new BorderLayout());
+
+      // Panels to display both hands and play area
+      pnlComputerHand = new JPanel(new GridLayout(1,numCardsPerHand)); //Remember col num is ignored if it goes over adds new column
+      pnlHumanHand = new JPanel(new GridLayout(1,numCardsPerHand)); //Remember col num is ignored
+      pnlPlayArea = new JPanel(new GridLayout(3,3)); //Remember col num is ignored
+      pnlTimerDisplay = new JPanel(new GridLayout(2,1)); // Timer display with Button
+
+      // Place panels to their specific location
+      add(pnlComputerHand, BorderLayout.NORTH);
+      add(pnlPlayArea, BorderLayout.CENTER);
+      add(pnlTimerDisplay, BorderLayout.WEST);
+      add(pnlHumanHand, BorderLayout.SOUTH);
+
+      // Add border titles to each section
+      pnlComputerHand.setBorder(new TitledBorder("Computer Hand"));
+      pnlPlayArea.setBorder(new TitledBorder("Playing Area"));
+      pnlTimerDisplay.setBorder(new TitledBorder("Timer Button"));
+      pnlHumanHand.setBorder(new TitledBorder("Your Hand"));
+
+      // Set the game text style
+      gameText.setText("Welcome to Build!");
+      gameText.setForeground(Color.red);
+      gameText.setHorizontalAlignment(JLabel.CENTER);
+
+      gameStatus.setText("Click on card to begin.");
+      gameStatus.setForeground(Color.red);
+      gameStatus.setHorizontalAlignment(JLabel.CENTER);
+
+      /**
+       * Create labels for each card in each panel
+       */
+      // Load the icons to use on cards
+      GUICard.loadCardIcons();
+
+      // Create labels for player and computer
+      for( int card = 0; card < NUM_CARDS_PER_HAND; card++)
+      {
+         // Load Cards Icons into array the cards
+         computerLabels[card] = new JLabel(GUICard.getIcon(highCardGame.getHand(0).inspectCard(card)));
+         playerCardButtons[card] = new JButton(GUICard.getIcon(highCardGame.getHand(1).inspectCard(card)));
+      }
+
+      // After creating labels above, you then add them to the specified panel
+      for( int card = 0; card < NUM_CARDS_PER_HAND; card++)
+      {
+         // Load cards from array into panel
+         pnlComputerHand.add(computerLabels[card]);
+         pnlHumanHand.add(playerCardButtons[card]);
+      }
+
+      // Create playing card Icons
+      playedCardLabels[0] = new JLabel(GUICard.getIcon(playerCard));
+      playedCardLabels[1] = new JLabel(GUICard.getIcon(computerCard));
+
+      // Create the text label under each played card
+      playerCardLabel = new JLabel("Pile 1", JLabel.CENTER);
+      computerCardLabel = new JLabel("Pile 2", JLabel.CENTER);
+
+      /**
+       * Add card icons to the play area section in table
+       */
+      // First Row
+      pnlPlayArea.add(playedCardLabels[0]);
+      pnlPlayArea.add(playedCardLabels[1]);
+      pnlPlayArea.add(gameText);
+
+      // Second Row
+      pnlPlayArea.add(playerCardLabel);
+      pnlPlayArea.add(computerCardLabel);
+      pnlPlayArea.add(gameStatus);
+
+      // Third Row
+      pnlPlayArea.add(cannotPlayButton);
+
+      // Add timer components to display
+      pnlTimerDisplay.add(startButton);
+      pnlTimerDisplay.add(timerDisplay);
+
+      // Show the table
+      setVisible(true);
+   }
+
+   // Set action listener for player cards
+   public void addCardListenerToCards(ActionListener cardListener, int card)
+   {
+      // Adds the action listener to specific card
+      playerCardButtons[card].addActionListener(cardListener);
+   }
+
+   public void addTimerButtonListener(ActionListener timerListener)
+   {
+      startButton.addActionListener(timerListener);
+   }
+
+   public void addNotPlayButtonListener(ActionListener buttonListener)
+   {
+      cannotPlayButton.addActionListener(buttonListener);
+   }
+
+   // Getter and Setters
+   public void setHighCardGame(CardGameFramework highCardGame)
+   {
+      this.highCardGame = highCardGame;
+   }
+
+   public JButton[] getPlayerCardButtons()
+   {
+      return playerCardButtons;
+   }
+
+   public void setComputerCard(Card computerCard)
+   {
+      this.computerCard = computerCard;
+   }
+
+   public JPanel getPnlHumanHand()
+   {
+      return pnlHumanHand;
+   }
+
+   public JLabel[] getPlayedCardLabels()
+   {
+      return playedCardLabels;
+   }
+
+   // Removes card from player and computer panels
+   public void removeCard(int index)
+   {
+      pnlComputerHand.remove(computerLabels[index]);
+      pnlHumanHand.remove(playerCardButtons[index]);
+      repaint();
+   }
+
+   // Updates the text on Game Status
+   public void setGameStatus(String str)
+   {
+      gameStatus.setText(str);
+   }
+
+   // Sets the player played card icon
+   public void setPlayerPlayedCardLabel(Card card)
+   {
+      this.playedCardLabels[0].setIcon(GUICard.getIcon(card));
+   }
+
+   // Sets the computer played card icon
+   public void setComputerPlayedCardLabel(Card card)
+   {
+      this.playedCardLabels[1].setIcon(GUICard.getIcon(card));
+   }
+
+   // Sets the computer played card to back icon
+   public void setComputerBackIcon()
+   {
+      this.playedCardLabels[0].setIcon(GUICard.getBackcardIcon());
+   }
+
+   // Set the seconds
+   public void setTimerDisplay(int seconds)
+   {
+      // Check to see if the seconds needs a leading zero
+      if( seconds%60 >= 0 && seconds%60 < 10)
+      {
+         this.timerDisplay.setText(Integer.toString(seconds/60) + ":0" + Integer.toString(seconds%60));
+      }
+      else
+      {
+         this.timerDisplay.setText(Integer.toString(seconds/60) + ":" + Integer.toString(seconds%60));
+      }
+   }
+
+   public JButton getStartButton()
+   {
+      return startButton;
+   }
+
+   public void setStartButtonText(String str)
+   {
+      this.startButton.setText(str);
+   }
+
+   public void setPlayerCard(Card playerCard)
+   {
+      this.playerCard = playerCard;
+   }
+
+   public JButton getCannotPlayButton()
+   {
+      return cannotPlayButton;
+   }
+
+   public void updateAllHandsTable()
+   {
+      pnlHumanHand.removeAll();
+      pnlComputerHand.removeAll();
+
+      // Create labels for player and computer
+      for( int card = 0; card < NUM_CARDS_PER_HAND; card++)
+      {
+         // Load Cards Icons into array the cards
+         playerCardButtons[card].setIcon(GUICard.getIcon(highCardGame.getHand(1).inspectCard(card)));
+         computerLabels[card].setIcon(GUICard.getIcon(highCardGame.getHand(0).inspectCard(card)));
+      }
+
+      // After creating labels above, you then add them to the specified panel
+      for( int card = 0; card < NUM_CARDS_PER_HAND; card++)
+      {
+         // Load cards from array into panel
+         pnlHumanHand.add(playerCardButtons[card]);
+         pnlComputerHand.add(computerLabels[card]);
+      }
+      repaint();
+   }
+}
