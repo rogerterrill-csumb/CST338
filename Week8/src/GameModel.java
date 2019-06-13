89f64fbe2020b373eaa9665c655fd776dc0ef38e
Roger Dodger <lpaddikt@gmail.com>
2019-06-12 22:53:11 -0700

    Setup the basic JFrame

--- Week8/src/GameModel.java
+++ Week8/src/GameModel.java
@@ -1,4 +1,8 @@
 class GameModel
 {
-   
+   // Global constants
+   static int NUM_PLAYERS = 2;
+
+   // Instance Members
+   private int dollars = 10;
 }
--- Week8/src/GameView.java
+++ Week8/src/GameView.java
@@ -5,5 +5,18 @@ import java.awt.event.ActionListener;
 
 class GameView extends JFrame
 {
+   // Default constructor
+   GameView()
+   {
+      // Sets the title of the JFrame
+      super("Blackjack");
 
+      // Basic Jframe foundation
+      setSize(800,700);
+      setLocationRelativeTo(null);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      // Display the JFrame
+      setVisible(true);
+   }
 }
